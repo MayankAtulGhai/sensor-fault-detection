e4c7d1fb8fac09f1ce7b8a4d170b7b3d1819073c
Mayank Ghai <mayankatulghai@gmail.com>
2022-12-08 02:22:43 +0530

    Here we have commit the first setup and the requirements.txt file

---
+++ sensor/__init__.py
@@ -0,0 +1,8 @@
+# any folder that we want to create as package or library should be created by the name __init__.py 
+
+# collection of libraries and packages are called a framework 
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+# setup.py is used to create packages, libarary can be shared by others or can be used for any other project
+
+# these are the basic need for us to start the project 
+
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements():
+    """ 
+    This function will return list of requirements
+    """
+    requirement_list = []
+    
+    '''
+    write a code to read requirement.txt and append each requirements in requirement_list variable.
+    '''
+
+    file = open('D:/Project/sensor-fault-detection/requirements.txt', 'rt')
+    requirement_list = file.readlines()
+    
+    return requirement_list
+
+setup(
+      name='sensor',
+      version='0.0.1',
+      author='mayank_ghai',
+      author_email='mayankatulghai@gmail.com',
+      packages=find_packages(),
+      install_requires=get_requirements(),
+)
+
+# after this run python setup.py install to setup the requirements by using setup.py file
+
+# or we can use pip install -r requirements.txt
