9cc647469c683da7b242f0509c99c4cdf0d81f77
MayankAtulGhai <mayankatulghai@gmail.com>
2022-12-08 17:16:52 +0530

    updated version

---
+++ main.py
@@ -0,0 +1,6 @@
+from sensor.confirguration.mongo_db_connection import MongoDBClient
+
+
+if __name__ == '__main__':
+    mongodb_client = MongoDBClient()
+    print("collection_name:",mongodb_client.database.list_collection_names())
--- sensor/constants/__init__.py
+++
@@ -1,1 +0,0 @@
-# in this all the constants that are being used in the projectsmi.e. it will contain all the variables that are being used.
